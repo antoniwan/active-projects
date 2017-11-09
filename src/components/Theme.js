e36d81277da7ffc22a42fb93c6fe2c4e91dc1169
Antonio Rodríguez <antoniwan@gmail.com>
2017-11-09 10:47:34 -0500

    add theme colors 🖤

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
+const ThemeColors = {
+  black: `#1b1725`,
+  green: `#7ddf64`,
+  red: `#ed254e`,
+  blue: `#01baef`,
+  yellow: `#f9dc5c`,
+  tan: `#d0cd94`,
+  indigo: `#22007c`
+};
+
 const Theme = styled.div`
   .container {
     max-width: 850px;
@@ -12,3 +22,4 @@ const Theme = styled.div`
   }
 `;
 export default Theme;
+export { ThemeColors };
